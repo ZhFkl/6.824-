2d4a66ea87aa653e23b007bbd07b962309c39723
li <lzhexpworld@gmail.com>
2026-08-17 17:56:57 +0800

    finish lab2 part 1

--- src/kvsrv1/client.go
+++ src/kvsrv1/client.go
@@ -1,11 +1,12 @@
 package kvsrv
 
 import (
-	"6.5840/kvsrv1/rpc"
-	"6.5840/kvtest1"
-	"6.5840/tester1"
-)
+	"time"
 
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
+	tester "6.5840/tester1"
+)
 
 type Clerk struct {
 	clnt   *tester.Clnt
@@ -15,6 +16,9 @@ type Clerk struct {
 func MakeClerk(clnt *tester.Clnt, server string) kvtest.IKVClerk {
 	ck := &Clerk{clnt: clnt, server: server}
 	// You may add code here.
+	// zai zhe li chuang jian ci shi de clerk , zhi hou ke yi tong guo chuang jian de
+	// clerk dui xiang lai diao yong ci shi de put he get han shu
+
 	return ck
 }
 
@@ -30,6 +34,58 @@ func MakeClerk(clnt *tester.Clnt, server string) kvtest.IKVClerk {
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 	// You will have to modify this function.
+
+	// zhe li shi get han shu
+	// shou xian chuang jian cis hide args de can shu
+	// zhe ge chan shu
+	/*
+
+		type GetArgs struct {
+		Key string
+		}
+
+		type GetReply struct {
+			Value   string
+			Version Tversion
+			Err     Err
+		}
+
+		can shu lei xing shi zheyang de , chuang jian yige kong de reply
+		dang args he reply chuang jian wan cheng zhi hou jiu ke yi diao yong ci shi de call han shu
+		ok := ck.clnt.Call(ck.server, "KVServer.Get", &args, &reply)
+
+		ran hou du qu ci shi reply zhong de zhi
+		zhe li yao you yige tiao jian pan duan
+		if ok != nil{
+		}else {
+
+		}
+
+	*/
+	//fmt.Printf("clint 开始 get \n")
+	args := rpc.GetArgs{
+		Key: key,
+	}
+	for {
+		var reply rpc.GetReply
+		ok := ck.clnt.Call(
+			ck.server,
+			"KVServer.Get",
+			&args,
+			&reply,
+		)
+
+		if ok {
+			switch reply.Err {
+			case rpc.OK:
+				return reply.Value, reply.Version, rpc.OK
+			case rpc.ErrNoKey:
+				return "", 0, rpc.ErrNoKey
+
+			}
+		}
+	}
+
 	return "", 0, rpc.ErrNoKey
 }
 
@@ -52,5 +108,48 @@ func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Put(key, value string, version rpc.Tversion) rpc.Err {
 	// You will have to modify this function.
-	return rpc.ErrNoKey
+	/*
+		type PutArgs struct {
+		Key     string
+		Value   string
+		Version Tversion
+		}
+
+		type PutReply struct {
+			Err Err
+		}
+		zhe li de laing ge can shu shi zheyang de , cishi de key he value yi jing gei ni le
+		zhi jie chu shi hua ci shi de putargs ran hou chuang jian yhige kong de reply , chuang jian wan cheng shi hou
+		diao yong huan shu
+		 ok := ck.clnt.Call(ck.server, "KVServer.Put", &args, &reply)
+
+		 zhe ge deng dai chu li , ran hou tong guo ok zhe ge zhi lai pan duan ci shi
+		 cao zuo you mei you cheng gong
+	*/
+	//fmt.Printf("clint 开始 put \n")
+	args := rpc.PutArgs{
+		Key:     key,
+		Value:   value,
+		Version: version,
+	}
+	retried := false
+	for {
+		var reply rpc.PutReply
+
+		ok := ck.clnt.Call(
+			ck.server,
+			"KVServer.Put",
+			&args,
+			&reply,
+		)
+		if ok {
+			if reply.Err == rpc.ErrVersion && retried {
+				return rpc.ErrMaybe
+			}
+			return reply.Err
+		}
+		retried = true
+		time.Sleep(100 * time.Millisecond)
+	}
+
 }
--- src/kvsrv1/server.go
+++ src/kvsrv1/server.go
@@ -6,7 +6,7 @@ import (
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labrpc"
-	"6.5840/tester1"
+	tester "6.5840/tester1"
 )
 
 const Debug = false
@@ -18,15 +18,26 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
+type Entry struct {
+	value   string
+	version rpc.Tversion
+}
 
 type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
+	// zhe li de KVServer xu yao zuo shen me
+	//xu yao yi map lai guan li ci shi de kv
+	// xu yao yi ge dong xi lai ji lu ci shi de version
+
+	data map[string]Entry
 }
 
 func MakeKVServer() *KVServer {
-	kv := &KVServer{}
+	kv := &KVServer{
+		data: make(map[string]Entry),
+	}
 	// Your code here.
 	return kv
 }
@@ -35,6 +46,30 @@ func MakeKVServer() *KVServer {
 // exists. Otherwise, Get returns ErrNoKey.
 func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 	// Your code here.
+	/*
+		args
+		Key string
+
+		reply
+		Value   string
+		Version Tversion
+		Err     Err
+		shou xian tong guo ci shi de key lai zhao value ru guo cun zai de hua na me ci shi jiu fan hui
+		dui ying de value , version , zhe ge version zen me fu zhi? wo de map zhong yao fang version ma ?
+		hai shi shuo ci shi version shi yige mask ?
+		shagn mian chu li wan cheng zhi hou  fan hui
+	*/
+	//fmt.Printf("服务器开始get\n")
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	entry, exists := kv.data[args.Key]
+	if !exists {
+		reply.Err = rpc.ErrNoKey
+		return
+	}
+	reply.Value = entry.value
+	reply.Err = rpc.OK
+	reply.Version = entry.version
 }
 
 // Update the value for a key if args.Version matches the version of
@@ -43,12 +78,73 @@ func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 // args.Version is 0, and returns ErrNoKey otherwise.
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
 	// Your code here.
+	// shou xian shi jin xing put cao zuo , dan shi zhe li put fang dao na ne ?
+	// suo yi ci shi wo de server xu yao guan li kV de yi ge shu ju jie gou
+	// huo zhe shuo shi map ,
+	// mei ci lai le zhi hou shou xina chong putargs zhong ti qu chu dui ying de key
+	/*
+		Key     string
+		Value   string
+		Version Tversion
+
+		map xian zhao key
+		1. key  bu cun zai na me ci shi jiu ba zhe ge kv jie gou cha ru dao mapp zhong
+		2. key cun zai dan shi value bu tong , ci shi jiu ba dui ying de value gai cheng xin de value
+		3. key cun zai er qie value ye xiang tong , na me ci shi zhe li de version shi zuo shen me de ?
+
+		map wan cheng zao zuo zhi hou
+		ci shi bu chong reply zhogn de shu ju
+		// Err's returned by server and Clerk
+		OK         = "OK"
+		ErrNoKey   = "ErrNoKey"
+		ErrVersion = "ErrVersion"
+
+		Err Err
+		ru guo cheng gong na me ci shi de err = OK
+		ru guo zhao bu dao de hua ci shi bu hui chuang jian ma?
+
+		// Update the value for a key if args.Version matches the version of
+		// the key on the server. If versions don't match, return ErrVersion.
+		// If the key doesn't exist, Put installs the value if the
+		// args.Version is 0, and returns ErrNoKey otherwise.
+		ru guo ci shi de version == 0 , na me jiu neng zeng jia
+		dan shi ru guo version == !0 na zen me ban ? you ban ben xie yi ma ?
+	*/
+	//fmt.Printf("服务器 kai shi put\n")
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	entry, exists := kv.data[args.Key]
+	if !exists {
+		if args.Version != 0 {
+			reply.Err = rpc.ErrNoKey
+			return
+		}
+		kv.data[args.Key] = Entry{
+			value:   args.Value,
+			version: 1,
+		}
+		reply.Err = rpc.OK
+		return
+	}
+
+	if args.Version != entry.version {
+		reply.Err = rpc.ErrVersion
+		return
+	}
+	entry.value = args.Value
+	entry.version++
+
+	kv.data[args.Key] = entry
+	reply.Err = rpc.OK
 }
-
-
 
 // You can ignore all arguments; they are for replicated KVservers
 func StartKVServer(tc *tester.TesterClnt, ends []*labrpc.ClientEnd, gid tester.Tgid, srv int, persister *tester.Persister) []any {
 	kv := MakeKVServer()
+
+	// zhe li MakerKVServer zhi hou ci shi yao zuo shen me ?
+	// yao yi zhi zai zhe li xun ran deng dai ci shi de client jin xing lian jie
+	// zhe li de xun huan zhi jie yong yi ge for ma ?
+	// hai you zai MakeKVServer zhong dui ci shi de server jin xing chu shi hua
 	return []any{kv}
 }
